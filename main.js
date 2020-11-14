86fb48a8b015f8b6f5f0e98679d6e99251ca2935
mohammad mekawy <mohammad.mekawy92@gmail.com>
2020-11-14 09:54:47 +0200

    first m

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    var arr = [];
+
+    for (i = 0; i < $("#journal").children().length; i++) {
+        arr.push($("#journal").children()[i].id)
+    }
+
+    $("#journal").change(function () {
+        var selectedJVal = $(this).children("option:selected").val();
+        $("#vol > option").each(function (i, item) {
+            $(this).prop("id", selectedJVal + `-${i+1}`)
+            $(this).prop("value", selectedJVal + `-${i+1}`)
+        })
+        $("#issue > option").each(function (i, item) {
+            var selectedVolVal = $("#vol").children("option:selected").val();
+            $(this).prop("id", selectedVolVal + `-${i+1}`);
+            $(this).prop("value", selectedVolVal + `-${i+1}`)
+        })
+    })
+
+    $("#vol").change(function () {
+        var selectedVolVal = $("#vol").children("option:selected").val();
+        $("#issue > option").each(function (i, item) {
+            $(this).prop("id", selectedVolVal + `-${i+1}`);
+            $(this).prop("value", selectedVolVal + `-${i+1}`)
+        })
+    })
+
+});
