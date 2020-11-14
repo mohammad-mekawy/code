8e89362d06b0ffd8eaf34498ff76bc57d07e9f35
mohammad mekawy <mohammad.mekawy92@gmail.com>
2020-11-14 10:50:47 +0200

    remove para

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ $(document).ready(function () {
 
     $("#journal").change(function () {
         var selectedJVal = $(this).children("option:selected").val();
-        $("#vol > option").each(function (i, item) {
+        $("#vol > option").each(function (i) {
             $(this).prop("id", selectedJVal + `-${i+1}`)
             $(this).prop("value", selectedJVal + `-${i+1}`)
         })
-        $("#issue > option").each(function (i, item) {
+        $("#issue > option").each(function (i) {
             var selectedVolVal = $("#vol").children("option:selected").val();
             $(this).prop("id", selectedVolVal + `-${i+1}`);
             $(this).prop("value", selectedVolVal + `-${i+1}`)
@@ -20,7 +20,7 @@ $(document).ready(function () {
 
     $("#vol").change(function () {
         var selectedVolVal = $("#vol").children("option:selected").val();
-        $("#issue > option").each(function (i, item) {
+        $("#issue > option").each(function (i) {
             $(this).prop("id", selectedVolVal + `-${i+1}`);
             $(this).prop("value", selectedVolVal + `-${i+1}`)
         })
